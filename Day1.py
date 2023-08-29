ad942826e0e97a0b354a5c29b3ad87ddfca548e7
Zainab Jahan <127616794+Sharktrix@users.noreply.github.com>
2023-08-29 17:27:31 +0000

    mnm

--- Day1.py
+++ Day1.py
@@ -1,8 +1,11 @@
 # Exercise 1 - Printing
 print("Day 1 - Python Print Function")
 print("The function is declared like this:")
-print("print('what to print')")
+print("print('what to print')\n\n")
 
 #Exerise 2 - Debugging
-print("Day 1 - String Manipulation\nString Concatenation is done with the "+" sign.")
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
 print('e.g. print("Hello" + "world")\nNew lines can be created with a backslash and n.')
+
+#Exercise 3 - Input Function
