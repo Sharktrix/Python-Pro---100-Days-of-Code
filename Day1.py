bc2d129d1c1aee37603556f4d615b07cb2d3d560
Zainab Jahan <127616794+Sharktrix@users.noreply.github.com>
2023-08-29 18:47:30 +0000

    Day1

--- Day1.py
+++ Day1.py
@@ -29,3 +29,25 @@ print(length)
 user_name = input("What is your name?")
 length1 = len(user_name)
 print(length1)
+
+# 3 
+
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+c = a 
+a = b 
+b = c
+
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
