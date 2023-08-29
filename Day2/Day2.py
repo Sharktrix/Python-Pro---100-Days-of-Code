2d35f6a6d5fc372bb3d5683ee4c4de369c3b688f
Zainab Jahan <127616794+Sharktrix@users.noreply.github.com>
2023-08-29 21:08:00 +0000

    Day2

--- Day2/Day2.py
+++ Day2/Day2.py
@@ -81,5 +81,28 @@ bmi_as_int = int(bmi)
 print(bmi_as_int)
 
 
+# Exercise 2.4  LIFE IN WEEKS
+
+age = input("What is your current age?")
+
+# Remember input always creates a string data type and we need to conver to int data type
+# 1 year = 365 Days 
+# 1 year = 52 Weeks 
+# 1 year = 12 Months 
+
+# Write your code here : 
+
+# Age is never going to be float (16.5)
+age_as_int = int(age)
+
+years_remaining = 90 - age_as_int
+days_remaining = years_remaining * 365 
+weeks_remaining = years_remaining * 52
+months_remaining = years_remaining * 12 
+
+message = f"You have {days_remaining} days, {weeks_remaining} weeks, and {months_remaining} months left."
+print(message)
+
+
 
 
