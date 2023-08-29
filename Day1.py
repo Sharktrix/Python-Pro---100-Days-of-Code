170989e665bd1dbee495bee519c32c0c88fb5686
Zainab Jahan <127616794+Sharktrix@users.noreply.github.com>
2023-08-29 18:15:43 +0000

    cnmn

--- Day1.py
+++ Day1.py
@@ -7,5 +7,13 @@ print("print('what to print')\n\n")
 print("Day 1 - String Manipulation")
 print('String Concatenation is done with the "+" sign.')
 print('e.g. print("Hello" + "world")\nNew lines can be created with a backslash and n.')
+print("Hello" + " " + "Shark\n\n")
 
-#Exercise 3 - Input Function
+#Exercise 3.1 - Input Function inside a Input function
+print("Hello " + input("What is your name?"))
+
+#Exercise 3.2 - Input Function inside a Input function
+input("What is your name?")
+
+#Exercise 3.3 - Input Function - To find length
+print(len ( input("What is your name? ") ) )
---
+++ Variables.py
@@ -0,0 +1,5 @@
+#Exercise 4 - Variables
+
+name = input("What is your name? ")
+length = len(name)
+print(length)
