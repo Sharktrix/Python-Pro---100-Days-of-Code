d39366807273724c0c157d66f2d9a97aa78aed97
Zainab Jahan <127616794+Sharktrix@users.noreply.github.com>
2023-09-10 16:31:51 +0000

    Day 3

--- Day3/Day3.md
+++ Day3/Day3.md
@@ -17,5 +17,47 @@ do this
 == Equal to 
 != Not Equal to 
 
+### Nested if statements and elif statements 
+
+> 120 cm 
+
+<= 18 $ 7 
+
+> 8 $ 12
+
+< 12 "5$" 
+
+12 - 18 "$7"
+
+> 18 "$ 12"
+
+#### Nested if / else
+
+```python 
+
+if condition:
+    if another condition:
+    do this 
+else:
+    do this
+
+else:
+    do this
+
+```
+
+### if / elif / else 
+
+```python
+
+if condition1:
+    do A
+elif condition2:
+    do B
+else : 
+    do this 
+
+```
+
 
 
--- Day3/Main.py
+++ Day3/Main.py
@@ -3,6 +3,48 @@ height = int(input("What is your height in cm?"))
 
 if height >= 120:
     print("You can ride the rollercoaster!")
+    age = int(input("What is your age?"))
+    if age < 12:
+        print("Please pay $5.")
+    elif age <= 18:
+        print("Please pay $7.")
+    else:
+        print("Please pay $12.")
 else:
     print("Sorry, you have to grow taller before you ride.")
+
+
+# Day 3-1 : Exercise 
+
+number = int(input("Which number do you want to change?"))
+
+if number % 2 == 0:
+    print("This is a even number.")
+else:
+    print("This is an odd number.")
+
+
+# Day 3-2 Exercise 
+
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+
+bmi = round( weight / height ** 2)
+if bmi < 18.5:
+    print(f"Your bmi is {bmi}, you are underweight. ")
+elif bmi < 25:
+    print(f"Your bmi is {bmi}, you have a normal weight. ")
+elif bmi < 30:
+    print(f"Your bmi is {bmi}, you are overweight.")
+elif bmi < 35:
+    print(f"Your bmi is {bmi}, you are obese.")
+else:
+    print(f"Your bmi is {bmi}, you are clinically obese.")
+
+
+
+
+
+
+
 
