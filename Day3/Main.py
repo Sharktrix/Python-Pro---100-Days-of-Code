98937815919df69bf3087375f2843e1907e1c2bb
Zainab Jahan <127616794+Sharktrix@users.noreply.github.com>
2023-09-20 17:56:02 +0000

    Day 3

--- Day3/Main.py
+++ Day3/Main.py
@@ -1,15 +1,26 @@
 print("Welcome to the Rollercoaster!")
 height = int(input("What is your height in cm?"))
+bill = 0
 
 if height >= 120:
     print("You can ride the rollercoaster!")
     age = int(input("What is your age?"))
     if age < 12:
-        print("Please pay $5.")
+        bill = 5
+        print("Child tickets are $5.")
     elif age <= 18:
-        print("Please pay $7.")
+        bill = 7
+        print("Youth tickets are $7.")
     else:
-        print("Please pay $12.")
+        bill = 12
+        print("Adult Tickets are $12.")
+
+    wants_photo = input("Do you want your photo taken? Y or N.")    
+    if wants_photo == "Y":
+        bill += 3
+    
+    print(f"Your final bill is ${bill}")
+
 else:
     print("Sorry, you have to grow taller before you ride.")
 
@@ -58,10 +69,32 @@ else:
     print("Not Leap year.")
 
 
+
+
+
 # Day 3.4 Exercise
 
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size do you want? S, M or L? ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
 
+bill = 0
 
+if size == "S":
+    bill += 15
+elif size == "M":
+    bill += 20
+else:
+    bill += 25
 
+if add_pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    else:
+        bill += 3
 
+if extra_cheese == "Y":
+    bill += 1
 
+print(f"Your final bill is ${bill}")
